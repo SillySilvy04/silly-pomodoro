4bf479fe8a4a9fb43877dc0f6c7123bccab6e5ab
silly_silvy <silvyespindola2004@gmail.com>
2026-01-10 01:00:57 -0300

    adição do componente contador

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './styles/global.css';
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/Countdown';
 
 export function App() {
   return (
@@ -14,6 +15,10 @@ export function App() {
 
       <Container>
         <Menu></Menu>
+      </Container>
+
+      <Container>
+        <CountDown></CountDown>
       </Container>
     </>
   );
